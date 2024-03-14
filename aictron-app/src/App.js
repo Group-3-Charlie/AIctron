fdae8886f65de224bd097c3b8ecc99bbd6f3f22e
Quentin Macheda <quentin.macheda@outlook.fr>
2024-03-14 21:47:36 +0000

    Finish routing set up

--- aictron-app/src/App.js
+++ aictron-app/src/App.js
@@ -1,9 +1,14 @@
 // App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from './components/Layout.js';
 import Home from './pages/Home.js';
-import Upload from './pages/Upload.js';
+import Upload from './pages/Predict/Upload.js';
+import ColumnChoice from './pages/Predict/ColumnChoice.js';
+import SelectorChoice from './pages/Predict/SelectorChoice.js';
+import NewValues from './pages/Predict/NewValues.js';
+import Result from './pages/Predict/Result.js';
 
 function App() {
   return (
@@ -11,9 +16,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="upload" element={<Upload />} />
-          <Route path="*" element={"ERROR 404 - NOT FOUND"} />
+          <Route path="predict">
+            <Route index element={<Upload />} />
+            <Route path="column-choice" element={<ColumnChoice />} />
+            <Route path="selector-choice" element={<SelectorChoice />} />
+            <Route path="new-values" element={<NewValues />} />
+            <Route path="result" element={<Result />} />
+          </Route>
         </Route>
+        <Route path="*" element={"ERROR 404 - NOT FOUND"} />
       </Routes>
     </BrowserRouter>
   );
