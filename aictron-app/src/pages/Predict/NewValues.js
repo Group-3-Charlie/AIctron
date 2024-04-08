7558def1a8f11d3359afbbe21c73f6159915e50b
Arnaud Michel <87248697+MrArnaudMichel@users.noreply.github.com>
2024-04-08 22:55:41 +0100

    NewValues.js integration

--- aictron-app/src/pages/Predict/NewValues.js
+++ aictron-app/src/pages/Predict/NewValues.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./css/NewValues.css";
 
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -8,28 +9,36 @@ const handleSubmit = (e) => {
 function handleFileChange(event) {
     const file = event.target.files[0];
     if (file) {
-      document.getElementById('importCSV').submit();
-      window.location.href = "/predict/result";
+        document.getElementById('importCSV').submit();
+        window.location.href = "/predict/result";
     }
 }
 
 
 const NewValues = () => {
-return (
-    <main className={"content-page-center"}>
-        <h2>Enter new values or Import CSV</h2>
-        <form action="" method="post" onSubmit={handleSubmit}>
-            <input type="text" name="input1" placeholder="Input 1"/>
-            <input type="text" name="input2" placeholder="Input 2"/>
-            <input type="text" name="input3" placeholder="Input 3"/>
-            <button className={"input-button"} type="submit">Submit</button>
-        </form>
+    return (
+        <main className={"content-page-center"}>
+            <h2>Enter new values or Import CSV</h2>
+            <p>Enter the values for 1 prediction <strong>or</strong> import a new (pre-filled) .csv file.</p>
+            <br/>
+            <div className={"content-page-content"}>
+                <form action="" method="post" onSubmit={handleSubmit} className={"input-form"}>
+                    <input type="text" name="input1" placeholder="Input 1"/>
+                    <input type="text" name="input2" placeholder="Input 2"/>
+                    <input type="text" name="input3" placeholder="Input 3"/>
+                    <button className={"input-button"} type="submit">Submit</button>
+                </form>
 
-        <form id="importCSV" action="backend/post-api" method="post" encType="multipart/form-data">
-            <input type="file" id="fileInput" name="file" accept=".csv" onChange={handleFileChange}/>
-        </form>
-    </main>
-);
+                <div className={"input-form"}>
+                    <p>If you need to get many results at once, import your (pre-filled) .csv file. </p>
+                    <form id="importCSV" encType="multipart/form-data" className="custom-file-input">
+                        <label htmlFor="fileInput">Choisir un fichier</label>
+                        <input type="file" id="fileInput" name="file" accept=".csv" onChange={handleFileChange}/>
+                    </form>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default NewValues;
