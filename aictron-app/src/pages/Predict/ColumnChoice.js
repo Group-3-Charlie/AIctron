2f5193aac7d00d910636755c09ad52f98c737f29
Quentin Macheda <quentin.macheda@outlook.fr>
2024-04-16 16:47:04 +0100

    Display the column names + Add button style + Add onClick selection

--- aictron-app/src/pages/Predict/ColumnChoice.js
+++ aictron-app/src/pages/Predict/ColumnChoice.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import "../../assets/style/column-choice.css";
 
 const ColumnChoice = () => {
   const [columns, setColumns] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedColumn, setSelectedColumn] = useState(-1);
+
+  const handleClick = (index) => {
+    setSelectedColumn(index);
+  };
 
   useEffect(() => {
     fetch('http://127.0.0.1:4567/get_columns', { method: 'GET' })
@@ -29,11 +35,14 @@ const ColumnChoice = () => {
       {isLoading ? (
         <p>Loading columns...</p>
       ) : (
-        <ul>
-          {columns.map((column, index) => (
-            <li key={index}>{column}</li>
-          ))}
-        </ul>
+        <>
+            <p>Choose the values's column you want to predict.</p>
+            <div id="columns-container">
+            {columns.map((column, index) => (
+                <button className={selectedColumn === index ? 'orange-btn selected' : 'orange-btn'} key={index} onClick={() => handleClick(index)} >{column}</button>
+            ))}
+            </div>
+        </>
       )}
 
       <form action="" method="post" onSubmit={handleSubmit}>
